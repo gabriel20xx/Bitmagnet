c8301f550cff58ae97fc9468975545664b30cdb4
Gabriel <gabrielfranz31@gmail.com>
2026-07-29 20:22:35 +0200

    fix: correct capitalization of 'Bitmagnet' in title and document title hook

--- webui/src/lib/hooks/useDocumentTitle.ts
+++ webui/src/lib/hooks/useDocumentTitle.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export function useDocumentTitle(...parts: Array<string | null | undefined>) {
   useEffect(() => {
-    document.title = [...parts.filter(Boolean), 'bitmagnet'].join(' - ')
+    document.title = [...parts.filter(Boolean), 'Bitmagnet'].join(' - ')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, parts)
 }
